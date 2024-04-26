43c91b341d10ad71f3ac4c7a089ccb726822191f
Herche Jane <53648352+herche-jane@users.noreply.github.com>
2024-04-26 18:47:52 +0800

    Revert "Refactor"

---
+++ build/lib/intelliscraper/test.py
@@ -0,0 +1,7 @@
+from intelliscraper.web_scraper import WebScraper
+
+wanted_list = ['北堂飘霜']
+scraper = WebScraper(wanted_list, url='https://blog.csdn.net/weixin_45487988?spm=1010.2135.3001.5343')
+results = scraper.build()
+for result in results:
+    print(result)
